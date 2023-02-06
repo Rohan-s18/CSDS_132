b09530b26fe0a05f238a245e4324119a0ab70104
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-06 16:00:21 -0500

    Added code for Trader

--- Spring_23/src/Project1/Trader.java
+++ Spring_23/src/Project1/Trader.java
@@ -1,5 +1,32 @@
 package Project1;
 
 public class Trader {
+
+    private String name;
+    private double balance;
+
+    public Trader(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void withdraw(double input){
+        balance-=input;
+    }
+
+    public void deposit(double input){
+        balance+=input;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
     
 }
