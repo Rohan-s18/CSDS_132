5ee219690e02c75e5df0dd7eab2a0f295d9c3b4c
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-06 18:31:48 -0500

    Finished Code for transaction class, except process

--- Spring_23/src/Project1/Transaction.java
+++ Spring_23/src/Project1/Transaction.java
@@ -2,4 +2,54 @@ package Project1;
 
 public class Transaction {
     
+    private char stockSymbol;
+    private int numShares;
+    private double price;
+    private Trader buyer;
+    private Trader seller;
+    private Market maker;
+    private boolean isClosed;
+
+    public Transaction(char stockSymbol, int numShares, double price, Trader buyer, Trader seller, Market maker){
+        this.stockSymbol = stockSymbol;
+        this.numShares = numShares;
+        this.price = price;
+        this.buyer = buyer;
+        this.seller = seller;
+        this.maker = maker;
+    }
+
+    public char getStockSymbol(){
+        return stockSymbol;
+    }
+
+    public int getNumberShares(){
+        return numShares;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public Trader getBuyer(){
+        return buyer;
+    }
+
+    public Trader getSeller(){
+        return seller;
+    }
+
+    public Market getMarketMaker(){
+        return maker;
+    }
+
+    public boolean isClosed(){
+        return isClosed;
+    }
+
+    public void processTransaction(){
+        
+    }
+
+
 }
