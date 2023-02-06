930606283f6a1853f5a55ccf70ffa5177eb0d06e
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-06 16:20:24 -0500

    Added Code for Order class

--- Spring_23/src/Project1/Order.java
+++ Spring_23/src/Project1/Order.java
@@ -1,5 +1,54 @@
 package Project1;
 
 public class Order {
+
+    private int numShares;
+    private double price;
+    private Trader trader;
+    private boolean allOrNone;
+    private char symbol;
+
+    public Order(int numShares, double price, Trader trader, boolean allOrNone, char symbol){
+        this.numShares = numShares;
+        this.allOrNone = allOrNone;
+        this.trader = trader;
+        this.symbol = symbol;
+        this.price = price;
+    }
+
+    public char getStockSymbol(){
+        return symbol;
+    }
+
+    public int getNumberShares(){
+        return numShares;
+    }
+
+    public void setNumberShares(int numShares){
+        this.numShares = numShares;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setAllOrNone(boolean allOrNone){
+        this.allOrNone = allOrNone;
+    }
+
+    public boolean isAllOrNone(){
+        return allOrNone;
+    }
+
+    public Trader getTrader(){
+        return trader;
+    }
+
+
+
     
 }
