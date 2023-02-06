c382047d4fb6ae38b9510907450ebea37022b16c
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-06 16:22:48 -0500

    Finished code for Buy/Sell orders

---
+++ Spring_23/src/Project1/BuyOrder.java
@@ -0,0 +1,9 @@
+package Project1;
+
+public class BuyOrder extends Order {
+
+    public BuyOrder(int numShares, double price, Trader trader, boolean allOrNone, char symbol){
+        super(numShares,price,trader,allOrNone,symbol);
+    }
+
+}
--- Spring_23/src/Project1/SellOrder.java
+++ Spring_23/src/Project1/SellOrder.java
@@ -1,5 +1,9 @@
 package Project1;
 
-public class SellOrder {
-    
+public class SellOrder extends Order {
+
+    public SellOrder(int numShares, double price, Trader trader, boolean allOrNone, char symbol){
+        super(numShares,price,trader,allOrNone,symbol);
+    }
+
 }
