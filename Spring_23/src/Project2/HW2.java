10ee55696450a5b6e231e7687f27a191fb5e6336
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-25 12:59:33 -0500

    finished foirst 2 methods

--- Spring_23/src/Project2/HW2.java
+++ Spring_23/src/Project2/HW2.java
@@ -3,12 +3,30 @@ package Project2;
 public class HW2{
 
     public static boolean matchingParentheses(String string){
-
-        return true;
+        int ct = 0;
+        boolean encountered = false;
+        for(int i = 0; i < string.length(); i++){
+            if(string.charAt(i) == '('){
+                ct++;
+                encountered = true;
+            }
+            else if(string.charAt(i) == ')')
+                ct--;
+        }
+        return (ct == 0 && encountered);
     }
 
     public static String everyNthExcept(String string, int start, int skip, int except){
-        return string;
+        start--;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(string.charAt(start));
+
+        for(int i = start; i < string.length(); i++){
+            if((i-start)%skip==0 && (i-start)%except!=0)
+                stringBuilder.append(string.charAt(i));
+        }
+
+        return stringBuilder.toString();
     }
 
     public static String flipEachK(String string){
@@ -31,7 +49,19 @@ public class HW2{
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("\n");
+
+        System.out.println(HW2.matchingParentheses("This is a (test (of the) (matching)) parentheses"));
+        System.out.println(HW2.matchingParentheses("The (second closing) parenthesis) does not match"));
+
+        System.out.println();
+
+        System.out.println(everyNthExcept("abcdefghijklmnop", 2, 2, 4));
+        System.out.println(everyNthExcept("abcdefghijklmnop", 3, 2, 4));
+
+        System.out.println("\n");
+
     }
 
 }
