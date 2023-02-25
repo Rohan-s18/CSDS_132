060a1177aa6e9110d29a9f0d34a2ba25c590ad29
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-25 12:38:43 -0500

    Added Skeletal Code

---
+++ Spring_23/src/Project2/HW2.java
@@ -0,0 +1,37 @@
+package Project2;
+
+public class HW2{
+
+    public static boolean matchingParentheses(String string){
+
+        return true;
+    }
+
+    public static String everyNthExcept(String string, int start, int skip, int except){
+        return string;
+    }
+
+    public static String flipEachK(String string){
+        return string;
+    }
+
+    public static String reverseDigits(String string){
+        return string;
+    }
+
+    public static String editOut(String string){
+        return string;
+    }
+
+    public static String replaceText(String string1, String string2){
+        return string1;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
